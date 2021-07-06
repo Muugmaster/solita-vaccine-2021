fdf4696fa9eb635dcb025d9b3574cde01d4275f1
Muukkonen <niko@muukkonen.info>
2021-07-06 14:59:08 +0300

    Create new controller function for querying data

--- server/controllers/orders.js
+++ server/controllers/orders.js
@@ -1,6 +1,9 @@
 const ordersRouter = require('express').Router()
 const { orders } = require('../models')
+const db = require('../models')
+const { Op } = require('sequelize')
 
+// Get all orders
 ordersRouter.get('/', async (req, res) => {
   try {
     const allOrders = await orders.findAll({})
@@ -10,6 +13,7 @@ ordersRouter.get('/', async (req, res) => {
   }
 })
 
+// Get orders by name of the vaccine
 ordersRouter.get('/vaccine', async (req, res) => {
   const { name } = req.query
 
@@ -32,6 +36,87 @@ ordersRouter.get('/vaccine', async (req, res) => {
         .json({ success: false, message: 'No orders with given params' })
     }
     return res.status(200).json({ success: true, orders: ordersByVaccineName })
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+// How many orders and vaccines have arrived total on given day?
+// How many orders/vaccines per producer?
+
+ordersRouter.get('/arrived', async (req, res) => {
+  const { date, producer } = req.query
+  console.log(date, producer)
+
+  if (!date) {
+    return res
+      .status(404)
+      .json({ success: false, message: 'Include search params' })
+  }
+
+  let formattedDate
+  try {
+    formattedDate = new Date(date)
+  } catch (error) {
+    return res
+      .status(404)
+      .json({ success: false, message: 'Search param need to be valid date' })
+  }
+
+  if (date && producer) {
+    try {
+      const sumOfOrdersAndVaccinationsPerProd = await orders.findAll({
+        attributes: [
+          [
+            db.sequelize.fn('SUM', db.sequelize.col('injections')),
+            'total_injections',
+          ],
+          [db.sequelize.fn('COUNT', db.sequelize.col('*')), 'total_orders'],
+        ],
+        where: {
+          [Op.and]: [
+            {
+              arrived: {
+                [Op.lt]: formattedDate,
+              },
+            },
+            { vaccine: producer },
+          ],
+        },
+      })
+
+      return res.status(200).json({
+        success: true,
+        date: formattedDate,
+        producer,
+        orders: sumOfOrdersAndVaccinationsPerProd,
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  try {
+    const sumOfOrdersAndVaccinations = await orders.findAll({
+      attributes: [
+        [
+          db.sequelize.fn('SUM', db.sequelize.col('injections')),
+          'total_injections',
+        ],
+        [db.sequelize.fn('COUNT', db.sequelize.col('*')), 'total_orders'],
+      ],
+      where: {
+        arrived: {
+          [Op.lt]: formattedDate,
+        },
+      },
+    })
+
+    return res.status(200).json({
+      success: true,
+      date: formattedDate,
+      orders: sumOfOrdersAndVaccinations,
+    })
   } catch (error) {
     console.log(error)
   }
--- server/controllers/vaccinations.js
+++ server/controllers/vaccinations.js
@@ -1,12 +1,53 @@
 const vaccinationsRouter = require('express').Router()
 const { vaccinations } = require('../models')
+const db = require('../models')
+const { Op } = require('sequelize')
 
+// Get all vaccination data
 vaccinationsRouter.get('/', async (req, res) => {
   try {
     const allVaccinations = await vaccinations.findAll({})
     return res
       .status(200)
       .json({ success: true, vaccinations: allVaccinations })
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+// How many of the vaccinations have been used for given day
+vaccinationsRouter.get('/used', async (req, res) => {
+  const { date } = req.query
+
+  if (!date) {
+    return res
+      .status(404)
+      .json({ success: false, message: 'Include search params' })
+  }
+
+  let formattedDate
+  try {
+    formattedDate = new Date(date)
+  } catch (error) {
+    return res
+      .status(404)
+      .json({ success: false, message: 'Search param need to be valid date' })
+  }
+
+  try {
+    const allUsedVaccinations = await vaccinations.findAll({
+      attributes: [
+        [db.sequelize.fn('COUNT', db.sequelize.col('*')), 'used_vaccinations'],
+      ],
+      where: {
+        vaccinationDate: {
+          [Op.lt]: formattedDate,
+        },
+      },
+    })
+    return res
+      .status(200)
+      .json({ success: true, vaccinations: allUsedVaccinations })
   } catch (error) {
     console.log(error)
   }
